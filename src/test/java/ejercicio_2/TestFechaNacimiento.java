27dfc255de20b2c7ebd509667b30db396e791535
MatiasEM12 <matiasemimellado@gmail.com>
2026-06-08 13:43:52 -0300

    ejercicio 2 test

--- src/test/java/ejercicio_2/TestFechaNacimiento.java
+++ src/test/java/ejercicio_2/TestFechaNacimiento.java
@@ -2,15 +2,35 @@ package ejercicio_2;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestFechaNacimiento {
 
     @Test
     public void formatoCorto(){
+        var hoy = LocalDate.now();
+        var persona = new Persona("pepe", hoy);
+        persona.asignarFormatoFecha(new FechaCorta());
 
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String fechaEsperada = hoy.format(formato);
+
+        assertEquals(fechaEsperada, persona.fechaNacimiento());
     }
 
     @Test
     public void formatoLargo(){
+        var hoy = LocalDate.now();
+        var persona = new Persona("pepe", hoy);
+        persona.asignarFormatoFecha(new FechaLarga());
 
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd 'de' MMMM 'de' yyyy", new Locale("es", "AR"));
+        String fechaEsperada = hoy.format(formato);
+
+        assertEquals(fechaEsperada, persona.fechaNacimiento());
     }
 }
